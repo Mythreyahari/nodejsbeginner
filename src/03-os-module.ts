7eeef83edfe7532663b6d456c60b156cc495a463
Mythreyahari <mythreyan53@gmail.com>
2026-07-18 19:42:03 +0530

    osmodule created and understand the concepts

---
+++ src/03-os-module.ts
@@ -0,0 +1,23 @@
+import * as os from "node:os";
+
+function runOsDemo():void{
+    console.log("platform",os.platform());
+    console.log("architecture",os.arch());
+    console.log("os type",os.type());
+    console.log("os release",os.release());
+    console.log("home directery",os.homedir());
+    console.log("temp directory",os.tmpdir());
+    
+    const cpus = os.cpus();
+    console.log(cpus.length);
+
+    if(cpus.length > 0){
+        console.log("first cpu",cpus[0].model,cpus[0].speed,cpus[0].times);
+        
+     }
+    
+    console.log(os.totalmem(),os.freemem());
+    
+};
+
+runOsDemo();
